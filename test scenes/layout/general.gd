bc72226463fff61558964b30853b3c61ee882696
adrianriverof <adrianriverof@gmail.com>
2024-10-20 07:07:52 +0200

    creando nueva sala 3. Refactor sistema de instancias de sala

--- test scenes/layout/general.gd
+++ test scenes/layout/general.gd
@@ -2,27 +2,31 @@ extends Spatial
 
 
 
-var room1scene = preload("res://test scenes/layout/1.tscn")
-var room2scene = preload("res://test scenes/layout/2.tscn")
+var room_scenes = [
+	preload("res://test scenes/layout/1.tscn"),
+	preload("res://test scenes/layout/2.tscn"),
+	preload("res://test scenes/layout/3.tscn")
+	# Añadir más escenas aquí...
+]
 
 
+func change_room_from_to(from, to):
+	print("venimos de la sala " + str(from) + " y vamos a la sala " + str(to))
 
+	# Verificar que el valor de 'to' esté dentro del rango de las salas disponibles
+	if to < 1 or to > room_scenes.size():
+		print("Error: Sala destino fuera de rango")
+		return
 
-func change_room_from_to(from,to):
-	print("venimos de la sala "+ str(from)+ " y vamos a la sala "+str(to))
+	# Instanciar la nueva sala basándote en el índice del array (ajustado por 'to - 1')
+	var roomtoinstance = room_scenes[to - 1].instance()
 	
-	var roomtoinstance
-	
-	match to:
-		1:
-			roomtoinstance = room1scene.instance()
-		2:
-			roomtoinstance = room2scene.instance()
-
 	roomtoinstance.from = from
-	#print(get_children())
+
+	# Elimina la sala actual
 	get_node("RoomManager").get_child(0).queue_free()
+	
+	# Añadir la nueva sala
 	get_tree().get_root().get_node("general").get_node("RoomManager").add_child(roomtoinstance, true)
-	
-	
+
 	
