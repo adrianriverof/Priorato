87181603e3738e7b657ebef238094f9497335b28
adrianriverof <adrianriverof@gmail.com>
2024-10-20 07:16:30 +0200

    enlazar sala 3

--- test scenes/layout/2.gd
+++ test scenes/layout/2.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 
 var from = 0
-
+var current_room = 2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -27,3 +27,13 @@ func _on_Area_body_entered(body):
 		#print(get_parent())
 		#print(get_parent().get_parent())
 		get_parent().get_parent().change_room_from_to(2,1)
+
+
+func _on_Puerta3_body_entered(body):
+	if body.name == $player.name:
+		print("player entra")
+		
+		#print(get_parent())
+		#print(get_parent().get_parent())
+		get_parent().get_parent().change_room_from_to(current_room,3)
+
