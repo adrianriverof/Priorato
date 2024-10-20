267ec1fdffd223fad33cb039948d7a1e7222054e
adrianriverof <adrianriverof@gmail.com>
2024-10-20 06:32:49 +0200

    añadir una segunda sala
    La madre cuanto he tardado en configurar camaras y tal

--- characters/player.gd
+++ characters/player.gd
@@ -12,7 +12,9 @@ var moving = false
 var is_dialog_active = false
 
 onready var camera = get_parent().get_node("Camera")
+onready var ball = get_parent().get_node("ball")
 onready var dialoguescene = preload("res://addons/dialogic/Dialog.tscn")
+
 
 
 func _ready():
@@ -22,29 +24,40 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		
+		print("pulso")
 		var ray_origin = camera.project_ray_origin(get_viewport().get_mouse_position())
 		var ray_direction = camera.project_ray_normal(get_viewport().get_mouse_position())
+		
+		
+		print(ray_direction)
 		
 		# Usamos una intersección para determinar donde está el suelo
 		var space_state = get_world().direct_space_state
 		var collision = space_state.intersect_ray(ray_origin, ray_origin + ray_direction * 1000, [self])
-		print(is_dialog_active)
+		#print(is_dialog_active)
 		if collision and !is_dialog_active:
+			print(collision.get("collider").name)
 			if collision.get("collider").name == "floor":
 				destination = collision.position
 				moving = true
+
 			elif  collision.get("collider").name == "compa":
 				print("has tocado a compa") #!!! hay que hacer la hitbox de compa mas grande entonces
 				
 				talk_to_compa()
-				print(is_dialog_active)
+				#print(is_dialog_active)
 				#print(Dialogic.load())
+
+
+
 
 func _physics_process(delta):
 	if moving:
 		var direction = (destination - global_transform.origin).normalized()
 		var distance_to_target = global_transform.origin.distance_to(destination)
-
+		
+		if ball: ball.translation = destination
+		
 		if distance_to_target > 0.5:
 			var motion = direction * speed * delta
 			
--- test scenes/layout/general.gd
+++ test scenes/layout/general.gd
@@ -1,19 +1,10 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-var room1scene = preload("res://test scenes/test_layout/1.tscn")
-var room2scene = preload("res://test scenes/test_layout/2.tscn")
+var room1scene = preload("res://test scenes/layout/1.tscn")
+var room2scene = preload("res://test scenes/layout/2.tscn")
 
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 
