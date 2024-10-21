15b7d020f192f375ac49b948eaa50fce523c16b8
adrianriverof <adrianriverof@gmail.com>
2024-10-21 05:32:48 +0200

    menu principal cierra y entra al juego

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,2 +1,10 @@
 extends Node2D
 
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://scenes/layout/general.tscn")
+
+func _on_CloseButton_pressed():
+	get_tree().quit()
+	#OS.kill(0)
