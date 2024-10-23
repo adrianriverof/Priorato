974654a510cc8da537bd65fbd9ab8f580205e554
adrianriverof <adrianriverof@gmail.com>
2024-10-23 06:11:10 +0200

    minijuego botella: movimiento del compa. Falta lo que sería cuenta atrás

--- scenes/minijuegos/botella.gd
+++ scenes/minijuegos/botella.gd
@@ -6,9 +6,22 @@ onready var player = $player
 
 
 func _ready():
-	pass # Replace with function body.
+	Input.warp_mouse_position(player.position)
 
 func _physics_process(delta):
-	var target_global_position: Vector2 = get_global_mouse_position() 
+	if Input.is_action_just_pressed("left_mouse"):
+		$compa/AnimationPlayer.play("open")
 	
-	
+
+
+func _on_lock_body_entered(body):
+	print("abierto")
+	if !$OpenWindow.is_stopped():
+		print("win")
+	else:
+		print("bad")
+		reset()
+
+
+func reset():
+	pass
