ea530107610010e8293e4ee66d7210af82fb22cd
adrianriverof <adrianriverof@gmail.com>
2024-10-24 05:01:32 +0200

    cambios postritual en 11

--- scenes/layout/11.gd
+++ scenes/layout/11.gd
@@ -4,11 +4,14 @@ extends Spatial
 var from = 0
 var current_room = 11
 
-
+onready var general = get_parent().get_parent()
 
 func _ready():
 	$RichTextLabel.text = "ROOM "+str(current_room)
 	#$escenario.visible = false
+	
+	change_after_ritual()
+	
 	match from:
 		
 		10:
@@ -23,10 +26,15 @@ func _on_Puerta10_body_entered(body):
 	if body.name == $player.name:
 		get_parent().get_parent().change_room_from_to(current_room,10)
 
-
-
-
 func _on_Puerta12_body_entered(body):
 	if body.name == $player.name:
 		get_parent().get_parent().change_room_from_to(current_room,12)
+
+
+func change_after_ritual():
+	if general.ritual_happened():
+		$Camera/PostRitual.visible = true
+		
+	else:
+		$Camera/PostRitual.visible = false
 
